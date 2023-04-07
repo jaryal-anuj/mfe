943847f674ecc21fa966fc34a26df2c635879a22
anuj <anuj@ourdesignz.in>
2023-04-07 17:27:41 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import MarketingApp from "./components/MarketingApp";
+export default ()=>{
+    return (
+        <div>
+            <h1>Hi container</h1>
+            <hr />
+            <MarketingApp />
+        </div>
+    );
+};
